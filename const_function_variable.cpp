ed86a6f89581c5941029ddc05a54863e8a2f4b02
Herman Lundkvist <herman.lundkvist@gmail.com>
2021-12-20 10:55:07 +0100

    Fix const_function_variable.cpp

--- const_function_variable.cpp
+++ const_function_variable.cpp
@@ -1,5 +1,7 @@
-void foo() {
-  const var{1};
+// This example shows that const objects may be placed on the stack (compile with -O0) 
+int foo() {
+  //int var{1};
+  int const var{1}; // same result as without const 
   return var;
 }
 
